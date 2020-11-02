9c110c28481f87d9500fee85853a88a7daee26e6
amrutha <ramrutharam98@gmail.com>
2020-11-02 23:22:18 +0530

    lab assignment

---
+++ Lab4_assignment2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Item video = new Video(1,"Movie",1,3);
+		System.out.println(video);
+		Item video1 = new Video();
+		
+	}
+
+}
